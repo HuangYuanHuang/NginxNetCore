8d07b15d94848ccba25cd1f43938b6518c81c88b
HuangYuanHuang <834773759@qq.com>
2018-01-11 17:08:38 +0800

    定时接口

---
+++ NetCoreMath/MathJob/MathCoreService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+namespace NetCoreMath.MathJob
+{
+    public interface IMathCoreService
+    {
+        void Excute();
+    }
+
+    public class MathCoreService : IMathCoreService
+    {
+        public void Excute()
+        {
+          //  Microsoft.AspNetCore.SignalR.Client.HubConnection hub = new HubConnection();
+            Console.WriteLine($"{DateTime.Now} 正在执行定时job");
+        }
+    }
+}
--- NetCoreMath/Startup.cs
+++ NetCoreMath/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Hangfire;
 using Hangfire.Mongo;
+using NetCoreMath.MathJob;
+
 namespace NetCoreMath
 {
     public class Startup
@@ -29,7 +31,7 @@ namespace NetCoreMath
 
             {
 
-           
+
                 var storageOptions = new MongoStorageOptions
 
                 {
@@ -46,27 +48,29 @@ namespace NetCoreMath
 
                 };
 
-               // config.UseMongoStorage("mongodb://root:Password01!@hyhrobot.com:27017", "Hangfire", storageOptions);
+                 config.UseMongoStorage("mongodb://hyhrobot.com:27017", "Hangfire", storageOptions);
 
             });
             services.AddMvc();
-          
+            services.AddSingleton<IMathCoreService, MathCoreService>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-           // GlobalConfiguration.Configuration.UseMongoStorage("mongodb://192.168.219.129:27017/?connectTimeoutMS=30000&maxIdleTimeMS=600000", "webrtc");
+            // GlobalConfiguration.Configuration.UseMongoStorage("mongodb://192.168.219.129:27017/?connectTimeoutMS=30000&maxIdleTimeMS=600000", "webrtc");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseHangfireServer();
             app.UseHangfireDashboard();
-         
-           
+
+
             app.UseMvc();
+            RecurringJob.AddOrUpdate<IMathCoreService>(d => d.Excute(), Cron.MinuteInterval(1));
+            
         }
     }
 }

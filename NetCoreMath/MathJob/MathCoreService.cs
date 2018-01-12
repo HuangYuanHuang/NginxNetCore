decda53bcc8696a6e9c6ae93e40a430eda0178de
HuangYuanHuang <834773759@qq.com>
2018-01-12 16:43:45 +0800

    Hub

--- NetCoreMath/MathJob/MathCoreService.cs
+++ NetCoreMath/MathJob/MathCoreService.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Sockets.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.SignalR.Internal.Protocol;
+
 namespace NetCoreMath.MathJob
 {
     public interface IMathCoreService
@@ -12,9 +16,27 @@ namespace NetCoreMath.MathJob
 
     public class MathCoreService : IMathCoreService
     {
+        static string SignalrUrl = "";
+        IHubConnectionBuilder nu;
+        IConnection connection; IHubProtocol protocol; ILoggerFactory loggerFactory;
+
+        HubConnection hub;
+        public MathCoreService()
+        {
+            connection = new HttpConnection(new Uri(""));
+            protocol = new JsonHubProtocol();
+            loggerFactory = new LoggerFactory();
+            hub = new HubConnection(connection, protocol, loggerFactory);
+            
+        }
+
+        public async Task Start()
+        {
+            await hub.StartAsync();
+        }
         public void Excute()
         {
-          //  Microsoft.AspNetCore.SignalR.Client.HubConnection hub = new HubConnection();
+
             Console.WriteLine($"{DateTime.Now} 正在执行定时job");
         }
     }
--- NetCoreSignalr/Startup.cs
+++ NetCoreSignalr/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetCoreSignalr.SignalrHub;
 
 namespace NetCoreSignalr
 {
@@ -23,6 +24,7 @@ namespace NetCoreSignalr
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +44,10 @@ namespace NetCoreSignalr
             }
 
             app.UseStaticFiles();
-
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<MessageHub>("messageHub");
+            });
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
